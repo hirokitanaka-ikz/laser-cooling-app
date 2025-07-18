b18f7f262b47409ae8f7cb2d4c22582d4fedec1b
Tanaka <tanaka@ad.ikz-berlin.de>
2025-07-18 18:18:01 +0200

    litmos data acquisition update

--- widgets/litmos_control_widget.py
+++ widgets/litmos_control_widget.py
@@ -1,11 +1,14 @@
 from PyQt6.QtWidgets import (
-    QGroupBox, QPushButton, QFileDialog, QMessageBox, QVBoxLayout
+    QGroupBox, QPushButton, QFileDialog, QMessageBox, QVBoxLayout, QFormLayout,
+    QDoubleSpinBox
 )
-from PyQt6.QtCore import QTimer
+from PyQt6.QtCore import QTimer, QThread, pyqtSignal
+import pyqtgraph as pg
 import csv
 import yaml
 from pathlib import Path
 import logging
+import time
 import datetime
 import sys
 
@@ -20,19 +23,57 @@ def default_filename() -> str:
 class LitmosControlPanel(QGroupBox):
 
     def __init__(self, parent=None):
-        super().__init__("LITMoS Control Panel", parent)
+        super().__init__("LITMoS Measurement Panel", parent)
 
         # UI Elements
-        self.start_btn = QPushButton("Start")
-        self.start_btn.clicked(self.toggle_start)
+        self.record_interval_spin = QDoubleSpinBox()
+        self.record_interval_spin.setRange(0.1, 60) # sec
+        self.record_interval_spin.setValue(1.0)
+        self.record_interval_spin.setDecimals(1)
+        self.record_interval_spin.setSuffix("sec")
+        self.record_btn = QPushButton("Start Record")
+        self.record_btn.clicked.connect(self.toggle_record)
+
+        self.rotator_btn = QPushButton("Run BRF rotation")
+        self.rotator_btn.clicked.connect(self.toggle_rotator)
+        self.rotator_start_spin = QDoubleSpinBox()
+        self.rotator_start_spin.setSuffix("°")
+        self.rotator_start_spin.setRange(0, 360)
+        self.rotator_start_spin.setDecimals(1)
+        self.rotator_stop_spin = QDoubleSpinBox()
+        self.rotator_stop_spin.setSuffix("°")
+        self.rotator_stop_spin.setRange(0, 360)
+        self.rotator_stop_spin.setDecimals(1)
+        self.rotator_step_spin = QDoubleSpinBox()
+        self.rotator_step_spin.setSuffix("°")
+        self.rotator_step_spin.setRange(0.01, 90)
+        self.rotator_stop_spin.setDecimals(1)
+
+        self.plot_widget = pg.PlotWidget()
+        self.plot_widget.setBackground("w")
+        self.plot_widget.showGrid(x=True, y=True)
+        self.plot_widget.setLabel("bottom", "Time", units="s")
+        self.plot_widget.setLabel("left", "Value")
+
+        # layout
+        record_form = QFormLayout()
+        record_form.addRow("Record Interval", self.record_interval_spin)
+        record_form.addWidget(self.record_btn)
+
+        filter_rotator_form = QFormLayout()
+        filter_rotator_form.addRow("Birefringent Filter Rotator", self.rotator_btn)
+        filter_rotator_form.addRow("Start Angle", self.rotator_start_spin)
+        filter_rotator_form.addRow("Stop Angle", self.rotator_stop_spin)
+        filter_rotator_form.addRow("Step Angle", self.rotator_step_spin)
 
         layout = QVBoxLayout()
-        layout.addWidget(self.start_btn)
-
+        layout.addLayout(record_form)
+        layout.addLayout(filter_rotator_form)
+        layout.addWidget(self.plot_widget)
         self.setLayout(layout)
 
     
-    def toggle_start(self):
+    def toggle_record(self):
         
         folder = QFileDialog.getExistingDirectory(self, "Select Save Destination Folder")
         if not folder:
@@ -57,4 +98,35 @@ class LitmosControlPanel(QGroupBox):
             yaml.dump(metadata, f_yml, allow_unicode=True)
 
         QMessageBox.information(self, "File Created", f"save path: \n{csv_path}\n{yml_path}\n\nmeasurement starting")
+        
+        # thread start here
 
+    def toggle_rotator(self):
+        pass
+
+
+class RecordThread(QThread):
+
+    ready = pyqtSignal(dict)
+
+    def __init__(self, spectrometer, interval, parent=None):
+        super().__init__(parent)
+        # self.data_collector
+        self.interval = interval
+        self._running = True
+
+    
+    def run(self):
+        while self._running:
+            try:
+                # prepare data here
+                # self.ready.emit(METHOD_NAME_HERE)
+                pass
+            except Exception as e:
+                logging.error(f"collecting data failed: {e}")
+            time.sleep(self.interval)
+
+
+    def stop(self):
+        self._running = False
+        self.wait()
